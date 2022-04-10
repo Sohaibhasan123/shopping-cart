47727bee5123bd1ddce04d756f567cd598ecc29e
Sohaibhasan123 <sohaibcsejust@gmail.com>
2022-04-10 19:38:44 +0600

    plus minus functionality added

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('I am in a separate file')
+document.getElementById("phone-plus").addEventListener('click',()=>{
+    const currentValue=getInput('phone');
+    let totalValue=currentValue+1;
+    document.getElementById("phone").value=`${totalValue}`;
+})
+document.getElementById("phone-minus").addEventListener('click',()=>{
+    const currentValue=getInput('phone');
+    let totalValue=currentValue-1;
+    document.getElementById("phone").value=`${totalValue}`;
+
+})
+document.getElementById("case-plus").addEventListener('click',()=>{
+    const currentValue=getInput('case');
+    let totalValue=currentValue+1;
+    document.getElementById("case").value=`${totalValue}`;
+})
+document.getElementById("case-minus").addEventListener('click',()=>{
+    const currentValue=getInput('case');
+    let totalValue=currentValue-1;
+    document.getElementById("case").value=`${totalValue}`;
+})
+function getInput(id)
+{
+    const currentValue=parseFloat(document.getElementById(id).value);
+    return currentValue;
+}
