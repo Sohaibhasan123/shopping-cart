dc227013d85b99f95f91bdfc004d03a4f06c99f5
Sohaibhasan123 <sohaibcsejust@gmail.com>
2022-04-11 11:54:31 +0600

    totalvalue added

--- app.js
+++ app.js
@@ -2,25 +2,54 @@ document.getElementById("phone-plus").addEventListener('click',()=>{
     const currentValue=getInput('phone');
     let totalValue=currentValue+1;
     document.getElementById("phone").value=`${totalValue}`;
+    calculateTotal();
 })
 document.getElementById("phone-minus").addEventListener('click',()=>{
-    const currentValue=getInput('phone');
-    let totalValue=currentValue-1;
+    let currentValue=getInput('phone');
+
+    
+        let totalValue=currentValue-1;
+  
+        
+    if(totalValue<0)
+    {
+        totalValue=0;
+    }
     document.getElementById("phone").value=`${totalValue}`;
+    calculateTotal();
 
 })
 document.getElementById("case-plus").addEventListener('click',()=>{
     const currentValue=getInput('case');
     let totalValue=currentValue+1;
     document.getElementById("case").value=`${totalValue}`;
+    calculateTotal();
 })
 document.getElementById("case-minus").addEventListener('click',()=>{
     const currentValue=getInput('case');
+    
     let totalValue=currentValue-1;
+    if(totalValue<0)
+    {
+        totalValue=0;
+    }
     document.getElementById("case").value=`${totalValue}`;
+    calculateTotal();
 })
 function getInput(id)
 {
-    const currentValue=parseFloat(document.getElementById(id).value);
+    const currentValue=parseInt(document.getElementById(id).value);
     return currentValue;
+}
+function calculateTotal()
+{
+   const phoneTotal=getInput("phone")*1219;
+   const caseTotal=getInput("case")*59;
+   const subTotal=phoneTotal+caseTotal;
+   const tax=subTotal/10;
+   const total=tax+subTotal;
+   document.getElementById("sub-total").innerText=subTotal;
+   document.getElementById("tax").innerText=tax;
+   document.getElementById("total").innerText=total;
+
 }
